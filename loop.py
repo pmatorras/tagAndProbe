500c4a652469ac0d2a99845a7aebf1eab45b3b8f
pmatorras <pablo.matorras@cern.ch>
2021-09-14 18:48:26 +0200

    add data and mc stuff

--- loop.py
+++ loop.py
@@ -16,12 +16,20 @@ year   = sys.argv[2].replace("20","")
 lep    = sys.argv[3]
 ylong  = "20"+year
 fol_name = ''
+
 if "e" in lep.lower(): lep = "Ele"
 if "m" in lep.lower(): lep = "Muon"
-if "data" in datamc.lower(): fol_name = "Run"+ylong+"_UL"+year+"_nAODv8_Full"+ylong+"v8/DataTandP__addTnP"+lep+"/"
+if "data" in datamc.lower(): fol_name = "Run"+ylong+"_UL"+ylong+"_nAODv8_Full"+ylong+"v8/DataTandP__addTnP"+lep+"/"
 elif "mc" in datamc.lower(): fol_name = "Summer20UL"+year+"_106x_nAODv8_Full"+ylong+"v8/MCTandP__addTnP"+lep+"/"
-if len(sys.argv)==4 : hfilenm = fol_name+"nanoLatino_DYJetsToLL_M-50_LO__part0.root"
-else : hfilenm  = fol_name+sys.argv[4]
+if len(sys.argv)==4 :
+    if "data" in datamc.lower():
+        if lep is "Ele": hfilenm  = fol_name+ "nanoLatino_SingleElectron_Run2017B-UL2017-v1__part0.root"
+        else: hfilenm = fol_name+"nanoLatino_SingleMuon_Run2017B-UL2017-v1__part0.root"
+
+    else:
+        hfilenm = fol_name+"nanoLatino_DYJetsToLL_M-50_LO__part0.root"
+else :
+    hfilenm  = fol_name+sys.argv[4]
 
 print fol_name, hfilenm
 hfile   = TFile(hfilenm,"READ","Example");
@@ -31,6 +39,7 @@ nEntries =  10000#events.GetEntries()
 #nEntries =  events.GetEntries()
 
 ptcut    = 20
+if lep is "Muon": ptcut=15
 etacut   = 2.4
 sip3Dcut = 4
 dxycut   = 0.05
@@ -46,8 +55,12 @@ for i in range(0, nEntries):
     lep_sip3D = events.Probe_sip3d
     lep_dxy   = events.Probe_dxy
     lep_dz    = events.Probe_dz
-    lep_cutB  = events.Probe_cutBased
-    if lep_cutB  < 3:                               continue
+    if   lep is "Ele" :
+        lep_cut  = events.Probe_cutBased
+        if lep_cut  < 3:                               continue
+    elif lep is "Muon":
+        lep_cut  = events.Probe_mediumId
+        if lep_cut !=1: continue
     Ncutb    += 1
 
     #print lep_pt, lep_eta, lep_dxy, lep_dz, lep_sip3D, lep_lostH, lep_cutB
@@ -56,18 +69,19 @@ for i in range(0, nEntries):
     if lep_dxy   > dxycut or lep_dz       > dzcut : continue
     if lep_sip3D < sip3Dcut:                        continue
 
-    eveHit = -1
-    if   lep_pt == events.Electron_pt[0] : eveHit = 0
-    elif lep_pt == events.Electron_pt[1] : eveHit = 1
+    if lep is "Ele":
+        eveHit = -1
+        if   lep_pt == events.Electron_pt[0] : eveHit = 0
+        elif lep_pt == events.Electron_pt[1] : eveHit = 1
+        lep_lostH = events.Electron_lostHits[eveHit]
+        
+        if lep_lostH != 0 :                      continue
+    elif lep is "Muon":
+        if events.Probe_miniPFRelIso_all > 0.15: continue
 
-    lep_lostH = events.Electron_lostHits[eveHit]
-    #print "lost hits", lep_lostH,"ele", events.Electron_lostHits
-
-    if lep_lostH != 0 :                             continue
     Nallcuts += 1
-    print i,":", events.event
-    #print lep_pt, lep_eta, lep_dxy, lep_dz, lep_sip3D, lep_lostH, lep_cutB
-
-    #if events.Probe_pt[0]>0: print "rrr", events.Probe_pt[0]
+    #print i,":", events.event,
+    #print lep_pt, lep_eta, lep_dxy, lep_dz, lep_sip3D, lep_cut
+    #print events.Probe_miniPFRelIso_all
 
 print "Cut based:\t",Ncutb,"\nAll cuts:\t",Nallcuts
